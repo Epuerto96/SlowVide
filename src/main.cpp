8decce9f4086f99288839cb02c4ab91d8c223d9a
Ethan Puerto <epuerto96@gmail.com>
2019-08-10 15:06:48 -0600

    Initial Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,70 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+// #include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+void updatetemps(float desiredTemp,float actualTemp) {
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Desired "));
+  display.setCursor(64,0);
+  display.println(F("Actual"));
+
+  display.setCursor(0,15);
+  display.setTextSize(2);             // Draw 3X-scale text
+  display.setTextColor(WHITE);
+  display.print(desiredTemp , 0);
+
+  display.setCursor(64,15);
+  display.setTextSize(2);             // Draw 3X-scale text
+  display.setTextColor(WHITE);
+  display.print(actualTemp, 1);
+
+  display.display();
+  delay(20);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float dTemp  = 130;
+  float temp = 130.1;
+  updatetemps(dTemp , temp);
 }
